6187d88a1470b5bc5b9fa07de89fed5d315197d9
redamoon <yuuki60824@gmail.com>
2024-03-19 23:54:20 +0900

    update DatePickers

--- components/VDatePickers/VDatePickers.vue
+++ components/VDatePickers/VDatePickers.vue
@@ -7,14 +7,26 @@ withDefaults(defineProps<{
   hideHeader?: boolean
   multiple?: boolean;
   isActions?: boolean;
+  bgColor?: string;
+  color?: string;
+  disabled?: boolean;
+  showAdjacentMonths?: boolean;
 }>(), {
-  multiple: false
+  multiple: false,
+  bgColor: undefined,
+  color: undefined,
+  disabled: false,
+  showAdjacentMonths: true
 })
 
 const emits = defineEmits<{
   'update:modelValue': [value: Date[] | Date]
 }>()
 
+const onClear = () => {
+  dateValue.value = []
+  emits('update:modelValue', dateValue.value)
+}
 const onSave = () => {
   emits('update:modelValue', dateValue.value)
 }
@@ -22,9 +34,18 @@ const onSave = () => {
 </script>
 
 <template>
-  <v-date-picker v-model="dateValue" :multiple :hide-header elevation="24">
+  <v-date-picker
+    v-model="dateValue"
+    :multiple
+    :hide-header
+    elevation="24"
+    :bg-color
+    :color
+    :disabled
+    :show-adjacent-months
+  >
     <template v-if="isActions" #actions>
-      <v-btn color="info" label="クリア" @click="dateValue = []">
+      <v-btn color="info" label="クリア" @click="onClear">
         クリア
       </v-btn>
       <v-btn color="primary" label="保存" @click="onSave">
