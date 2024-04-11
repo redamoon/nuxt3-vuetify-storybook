dafe9a50584956117b722ff1b908ac49553fdff7
redamoon <yuuki60824@gmail.com>
2024-04-11 18:24:44 +0900

    nuxt update 日付の範囲

---
+++ components/VPeriodDatePickers/VPeriodDatePickers.vue
@@ -0,0 +1,94 @@
+<script setup lang="ts">
+import { computed, ref, unref } from 'vue'
+
+const range = ref([])
+const menu = ref(false)
+const model = defineModel<Date[]>()
+const dates = computed({
+  get: datesBetween,
+  set: updateRange
+})
+
+const dateRangeText = computed(() => {
+  if (model.value === undefined || model.value.length === 0) {
+    return '登録した日付が入ります。'
+  }
+  const [start, end] = unref(model)
+  if (!!start && !!end) {
+    return `${start.toLocaleDateString()}
+      ~ ${end.toLocaleDateString()}`
+  } else if (isToday(start)) {
+    return 'テスト'
+  } else {
+    return start.toLocaleDateString()
+  }
+})
+
+function updateRange (date) {
+  const [start, end] = unref(range)
+  // if everything is null or everything is not
+  if (!!start === !!end) {
+    range.value = [date]
+  } else if (date < start) {
+    range.value = [date, start]
+  } else if (date > start) {
+    range.value = [start, date]
+  }
+}
+
+function datesBetween () {
+  if (range.value === undefined) {
+    return []
+  }
+  const [start, end] = unref(range)
+  if (!!start && !!end) {
+    const between = []
+    const currentDate = new Date(start)
+    while (currentDate <= end) {
+      between.push(new Date(currentDate))
+      currentDate.setDate(currentDate.getDate() + 1)
+    }
+    return between
+  } else {
+    return range.value
+  }
+}
+
+function isToday (date) {
+  const today = new Date(Date.now())
+  today.setHours(0, 0, 0, 0)
+  return today.getTime() === date.getTime()
+}
+
+function save () {
+  model.value = range.value
+  menu.value = false
+}
+
+function cancel () {
+  range.value = model.value || []
+  menu.value = false
+}
+</script>
+
+<template>
+  <div>
+    <p>{{ dateRangeText }}</p>
+    <v-date-picker
+      v-model="dates"
+      hide-header
+      class="datePicker mt-4"
+      show-adjacent-months
+      show-current
+      scrollable
+      rounded
+      elevation="5"
+      :max="new Date()"
+    >
+      <template #actions>
+        <v-btn color="primary" variant="text" text="cancel" @click="cancel" />
+        <v-btn color="primary" variant="text" text="OK" @click="save" />
+      </template>
+    </v-date-picker>
+  </div>
+</template>
